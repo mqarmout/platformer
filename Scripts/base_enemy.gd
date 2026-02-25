ac3859601b996615a9b2c21bb7121371ab64148f
mqarmout <majedamf@gmail.com>
2026-02-24 21:56:02 -0500

    started attempting to detect floor

--- Scripts/base_enemy.gd
+++ Scripts/base_enemy.gd
@@ -5,7 +5,8 @@ const JUMP_VELOCITY = -160.0
 var direction:int = -1
 var can_move:bool = true
 var timer = Timer.new()
-@export var ray:RayCast2D
+@export var front_ray:RayCast2D
+@export var floor_ray:RayCast2D
 
 func jump() -> void:
 	if is_on_floor():
@@ -25,11 +26,13 @@ func facing_wall() -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if ray.is_colliding():
-		if ray.get_collider().name == "Player":
+	if front_ray.is_colliding():
+		if front_ray.get_collider().name == "Player":
 			print("follow the player")
-		elif ray.get_collider().name == "Map_Manager":
+		elif front_ray.get_collider().name == "Map_Manager":
 			facing_wall()
+	if !floor_ray.is_colliding():
+		print("there is no floor!")
 	
 	if can_move:
 		move(direction)
